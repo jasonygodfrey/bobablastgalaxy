45de69feeb0c7cb9197b305c0e1e6b964667a2b1
Jason <jasonygodfrey@gmail.com>
2023-07-03 08:45:03 -0700

    ship1

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -31,7 +31,33 @@ class GameScene extends Phaser.Scene {
 
     create() {
         this.editorCreate();
+    
+        // Create the player spaceship
+        this.player = this.add.sprite(640, 600, 'ship1');
+        this.player.setScale(0.5); // Adjust the scale to make it smaller
+    
+        // Enable input for the player spaceship
+        this.player.setInteractive();
+        this.input.setDraggable(this.player);
+    
+        // Register the pointermove event to update the player spaceship's position
+        this.input.on('pointermove', (pointer) => {
+            this.player.x = pointer.x;
+            this.player.y = pointer.y;
+        });
+    
+        // Hide the mouse cursor when over the player spaceship
+        this.input.on('pointerover', () => {
+            document.body.style.cursor = 'none';
+        });
+    
+        // Show the mouse cursor when not over the player spaceship
+        this.input.on('pointerout', () => {
+            document.body.style.cursor = 'default';
+        });
     }
+    
+    
 
     update() {
         // Scroll the background slower
