f8365236e2efbf44a19a03dcc9d856997ef9980d
Jason <jasonygodfrey@gmail.com>
2023-07-04 12:13:10 -0700

    stop player projectiles on game over

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -8,8 +8,11 @@ class GameScene extends Phaser.Scene {
 
         // Initialize the player's health
         this.playerHealth = 10;
-            // Flag to track if player ship has been hit in the current frame
-    this.playerHit = false;
+        // Flag to track if player ship has been hit in the current frame
+        this.playerHit = false;
+
+        // Flag to track game over state
+        this.isGameOver = false;
 
         // Write your code here.
         /* END-USER-CTR-CODE */
@@ -19,31 +22,35 @@ class GameScene extends Phaser.Scene {
         // Create the enemy projectile
         const projectile = this.physics.add.sprite(enemy.x, enemy.y + 50, 'purplejelly');
         projectile.setScale(0.2); // Adjust the scale if necessary
-    
+
         // Add the enemy projectile to the enemyProjectiles group
         this.enemyProjectiles.add(projectile);
-    
+
         // Set the speed of the enemy projectile
         const projectileSpeed = 250; // Adjust the speed as needed
-    
+
         // Calculate the distance the projectile needs to travel to move off screen
         const distance = this.sys.game.config.height - (enemy.y + 50);
-    
+
         // Move the projectile downwards
         this.tweens.add({
-          targets: projectile,
-          y: `+=${distance}`, // move downwards off screen
-          angle: 360, // rotate 360 degrees
-          duration: 1000 * (distance / projectileSpeed), // time based on speed
-          onComplete: () => {
-            projectile.destroy(); // destroy the projectile when it goes off screen
-          }
+            targets: projectile,
+            y: `+=${distance}`, // move downwards off screen
+            angle: 360, // rotate 360 degrees
+            duration: 1000 * (distance / projectileSpeed), // time based on speed
+            onComplete: () => {
+                projectile.destroy(); // destroy the projectile when it goes off screen
+            }
         });
-      }
-      
+    }
+
 
 
     shootProjectile() {
+
+        if (this.isGameOver) {
+            return; // Do not shoot projectiles if the game is over
+          }
         // Create the projectile
         let projectile = this.physics.add.sprite(this.player.x, this.player.y - 50, 'boba1');
         projectile.setScale(0.2); // Adjust the scale of the projectile if necessary
@@ -106,6 +113,9 @@ class GameScene extends Phaser.Scene {
     preload() {
         this.load.pack("assetPack", "assets/asset-Pack.json");
 
+
+
+
         // Load the sprite sheet with new frame dimensions
         this.load.spritesheet('sparkle1', 'assets/sparkle1spritesheet.png', { frameWidth: 33.2, frameHeight: 60 });
     }
@@ -116,10 +126,10 @@ class GameScene extends Phaser.Scene {
 
         this.input.setDefaultCursor('none');
 
-        
 
-            // Create the enemy projectiles group with physics
-    this.enemyProjectiles = this.physics.add.group();
+
+        // Create the enemy projectiles group with physics
+        this.enemyProjectiles = this.physics.add.group();
 
         // Create an animation from the new sprite sheet
         this.anims.create({
@@ -135,8 +145,8 @@ class GameScene extends Phaser.Scene {
         this.player = this.add.sprite(640, 600, 'ship1');
         this.player.setScale(0.5); // Adjust the scale to make it smaller
 
-            // Enable physics for the player sprite
-    this.physics.world.enable(this.player);
+        // Enable physics for the player sprite
+        this.physics.world.enable(this.player);
 
         // Add the sparkle animation as the 'exhaust' of the player ship
         this.playerExhaust = this.add.sprite(this.player.x, this.player.y + 90, 'sparkle1');
@@ -250,6 +260,7 @@ class GameScene extends Phaser.Scene {
             loop: true
         });
 
+
         // Start the enemy shooting timer
         this.time.addEvent({
             delay: 2000,
@@ -265,121 +276,161 @@ class GameScene extends Phaser.Scene {
         });
 
 
-      // Enable collision between projectiles and enemies
-this.physics.add.collider(this.projectiles, this.enemies, (projectile, enemy) => {
-    if (!enemy.hitByProjectile && !enemy.isShooting) {
-      this.destroyEnemy(projectile, enemy);
-      enemy.hitByProjectile = true;
+        // Enable collision between projectiles and enemies
+        this.physics.add.collider(this.projectiles, this.enemies, (projectile, enemy) => {
+            if (!enemy.hitByProjectile && !enemy.isShooting) {
+                this.destroyEnemy(projectile, enemy);
+                enemy.hitByProjectile = true;
+            }
+        });
+
+        // Create the enemy projectiles group with physics
+        this.enemyProjectiles = this.physics.add.group();
+
+        // Enable collision between enemy projectiles and the player
+        this.physics.add.collider(this.enemyProjectiles, this.player, (player, projectile) => {
+            if (!this.playerHit) {
+                projectile.destroy(); // Destroy the enemy projectile
+
+                // Decrease the player's health
+                this.playerHealth -= 1;
+
+                // Update the health text
+                this.healthText.setText(`BloodSugar: ${this.playerHealth}`);
+
+                //GAMEOVER GAME OVER LOGIC :):):):)
+
+                if (this.playerHealth <= 0) {
+                    // Player is defeated, show game over screen
+                    const centerX = this.sys.game.config.width / 2;
+                    const centerY = this.sys.game.config.height / 2;
+                    this.add.image(centerX, centerY, 'gameover').setScale(1.5).setOrigin(0.5).setDepth(10000); // Set high depth value
+
+
+                    // Stop all ongoing tweens and physics
+                    this.physics.pause();
+                    this.tweens.pauseAll();
+                          // Set the game over flag to true
+      this.isGameOver = true;
+
+
+
+
+                    // Make the player inactive and visible
+                    this.player.setActive(false).setVisible(false);
+                    this.input.setDefaultCursor('url(assets/starcursor.png), pointer');
+
+                    // Optionally, you can add a timed event that will restart the game (scene)
+                    // after a few seconds of showing the 'game over' image
+                    //this.time.delayedCall(3000, () => this.scene.restart(), [], this);
+                }
+
+
+                this.playerHit = true; // Set the flag to true to prevent multiple collisions within the same frame
+
+                // Flash the player sprite to indicate getting hit
+                this.tweens.add({
+                    targets: this.player,
+                    alpha: 0.5, // Set the alpha to a lower value temporarily
+                    duration: 100,
+                    yoyo: true,
+                    repeat: 3,
+                    onComplete: () => {
+                        this.player.alpha = 1; // Reset the alpha value to 1
+                    }
+                });
+            }
+        }, null, this);
+
+
+
+
+
     }
-  });
-  
-  // Create the enemy projectiles group with physics
-  this.enemyProjectiles = this.physics.add.group();
-  
-// Enable collision between enemy projectiles and the player
-this.physics.add.collider(this.enemyProjectiles, this.player, (player, projectile) => {
-    if (!this.playerHit) {
-        projectile.destroy(); // Destroy the enemy projectile
 
-        // Decrease the player's health
-        this.playerHealth -= 1;
 
-        // Update the health text
-        this.healthText.setText(`BloodSugar: ${this.playerHealth}`);
 
-        if (this.playerHealth <= 0) {
-            // Player is defeated, handle game over logic
-        }
 
-        this.playerHit = true; // Set the flag to true to prevent multiple collisions within the same frame
+    update() {
+        // Update the position of the exhaust to follow the player ship
+        this.playerExhaust.x = this.player.x;
+        this.playerExhaust.y = this.player.y + 110;
 
-        // Flash the player sprite to indicate getting hit
-        this.tweens.add({
-            targets: this.player,
-            alpha: 0.5, // Set the alpha to a lower value temporarily
-            duration: 100,
-            yoyo: true,
-            repeat: 3,
-            onComplete: () => {
-                this.player.alpha = 1; // Reset the alpha value to 1
+        // Scroll the background slower
+        this.background.tilePositionY -= 0.6;
+
+        // Reset the playerHit flag in each update frame
+        this.playerHit = false;
+
+        // Update the health bar
+        const healthBarWidth = 200;
+        const healthPercentage = this.playerHealth / 10; // Convert the health to a percentage value
+        this.healthBar.setScale(healthPercentage, 1); // Adjust the health bar scale based on the health percentage
+
+        // Handle collision between projectiles and enemies
+        this.physics.add.collider(this.projectiles, this.enemies, (projectile, enemy) => {
+            if (!enemy.hitByProjectile && !enemy.isShooting) {
+                enemy.hitByProjectile = true;
+                this.destroyEnemy(projectile, enemy);
+            }
+        });
+
+        // Handle collision between enemy projectiles and the player
+        this.physics.add.collider(this.enemyProjectiles, this.player, (player, projectile) => {
+            if (!this.playerHit) {
+                projectile.destroy(); // Destroy the enemy projectile
+
+                // Decrease the player's health
+                this.playerHealth -= 1;
+
+                // Update the health text
+                this.healthText.setText(`BloodSugar: ${this.playerHealth}`);
+
+                if (this.playerHealth <= 0) { // Modified here
+                    console.log("Game over triggered");
+                    // Player is defeated, show game over screen
+                    const centerX = this.sys.game.config.width / 2;
+                    const centerY = this.sys.game.config.height / 2;
+                    this.add.image(centerX, centerY, 'gameover').setScale(0.5).setOrigin(0.5);
+
+                    // Stop all ongoing tweens and physics
+                    this.physics.pause();
+                    this.tweens.pauseAll();
+
+                    // Make the player inactive and visible
+                    this.player.setActive(false).setVisible(false);
+
+                    // Optionally, you can add a timed event that will restart the game (scene)
+                    // after a few seconds of showing the 'game over' image
+                    this.time.delayedCall(3000, () => this.scene.restart(), [], this);
+                }
+
+                this.playerHit = true; // Set the flag to true to prevent multiple collisions within the same frame
+
+                // Flash the player sprite to indicate getting hit
+                this.tweens.add({
+                    targets: this.player,
+                    alpha: 0.5, // Set the alpha to a lower value temporarily
+                    duration: 100,
+                    yoyo: true,
+                    repeat: 3,
+                    onComplete: () => {
+                        this.player.alpha = 1; // Reset the alpha value to 1
+                    }
+                });
+            }
+        });
+
+
+        // Handle collision between enemy projectiles and the player (queued processing)
+        this.physics.world.colliders.getActive().forEach((collider) => {
+            if (collider.bodyA === this.player.body || collider.bodyB === this.player.body) {
+                collider.collideCallback(collider.bodyA === this.player.body ? collider.bodyB.gameObject : collider.bodyA.gameObject);
             }
         });
     }
-}, null, this);
 
 
-
-
-
-}
-
-
-
-
-update() {
-    // Update the position of the exhaust to follow the player ship
-    this.playerExhaust.x = this.player.x;
-    this.playerExhaust.y = this.player.y + 110;
-
-    // Scroll the background slower
-    this.background.tilePositionY -= 0.6;
-
-    // Reset the playerHit flag in each update frame
-    this.playerHit = false;
-
-    // Update the health bar
-    const healthBarWidth = 200;
-    const healthPercentage = this.playerHealth / 10; // Convert the health to a percentage value
-    this.healthBar.setScale(healthPercentage, 1); // Adjust the health bar scale based on the health percentage
-
-    // Handle collision between projectiles and enemies
-    this.physics.add.collider(this.projectiles, this.enemies, (projectile, enemy) => {
-        if (!enemy.hitByProjectile && !enemy.isShooting) {
-            enemy.hitByProjectile = true;
-            this.destroyEnemy(projectile, enemy);
-        }
-    });
-
-    // Handle collision between enemy projectiles and the player
-    this.physics.add.collider(this.enemyProjectiles, this.player, (player, projectile) => {
-        if (!this.playerHit) {
-            projectile.destroy(); // Destroy the enemy projectile
-
-            // Decrease the player's health
-            this.playerHealth -= 1;
-
-            // Update the health text
-            this.healthText.setText(`BloodSugar: ${this.playerHealth}`);
-
-            if (this.playerHealth <= 0) {
-                // Player is defeated, handle game over logic
-            }
-
-            this.playerHit = true; // Set the flag to true to prevent multiple collisions within the same frame
-
-            // Flash the player sprite to indicate getting hit
-            this.tweens.add({
-                targets: this.player,
-                alpha: 0.5, // Set the alpha to a lower value temporarily
-                duration: 100,
-                yoyo: true,
-                repeat: 3,
-                onComplete: () => {
-                    this.player.alpha = 1; // Reset the alpha value to 1
-                }
-            });
-        }
-    });
-
-    // Handle collision between enemy projectiles and the player (queued processing)
-    this.physics.world.colliders.getActive().forEach((collider) => {
-        if (collider.bodyA === this.player.body || collider.bodyB === this.player.body) {
-            collider.collideCallback(collider.bodyA === this.player.body ? collider.bodyB.gameObject : collider.bodyA.gameObject);
-        }
-    });
-}
-
-    
 
     /* END-USER-CODE */
 }
